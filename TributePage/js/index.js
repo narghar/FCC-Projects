396a29c1859ed395906558a46918f7f4488a39d9
Igor Wysocki <narghar@gmail.com>
2019-05-24 23:55:15 +0200

    add timeline animation

---
+++ TributePage/js/index.js
@@ -0,0 +1,36 @@
+// !! IMPORTANT README:
+
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place.
+
+/***********
+INSTRUCTIONS:
+  - Select the project you would
+    like to complete from the dropdown
+    menu.
+  - Click the "RUN TESTS" button to
+    run the tests against the blank
+    pen.
+  - Click the "TESTS" button to see
+    the individual test cases.
+    (should all be failing at first)
+  - Start coding! As you fulfill each
+    test case, you will see them go
+    from red to green.
+  - As you start to build out your
+    project, when tests are failing,
+    you should get helpful errors
+    along the way!
+    ************/
+
+// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+
+// Once you have read the above messages, you can delete all comments.
+AOS.init({
+  offset: 170, // offset (in px) from the original trigger point
+delay: 0, // values from 0 to 3000, with step 50ms
+duration: 1200, // values from 0 to 3000, with step 50ms
+easing: 'ease', // default easing for AOS animations
+once: false, // whether animation should happen only once - while scrolling down
+mirror: true, // whether elements should animate out while scrolling past them
+
+});
