744de9baed766d08b0af857c68fc608ebbdcfeec
Igor Wysocki <narghar@gmail.com>
2019-05-28 23:35:52 +0200

    change animation behavior

--- TributePage/js/index.js
+++ TributePage/js/index.js
@@ -25,12 +25,13 @@ INSTRUCTIONS:
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
 // Once you have read the above messages, you can delete all comments.
-AOS.init({
-  offset: 170, // offset (in px) from the original trigger point
-delay: 0, // values from 0 to 3000, with step 50ms
-duration: 1200, // values from 0 to 3000, with step 50ms
-easing: 'ease', // default easing for AOS animations
-once: false, // whether animation should happen only once - while scrolling down
-mirror: true, // whether elements should animate out while scrolling past them
+const settings = {
+  offset: 0, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 600, // values from 0 to 3000, with step 50ms
+  easing: 'ease-out', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' };
 
-});
+AOS.init(settings);
